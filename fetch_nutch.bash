ab0ab9b2ca0f6709267b67ce77b50d99603c607a
Gerardo Porras <gporras@yahoo-inc.com>
2017-10-17 14:48:44 -0700

    Fixing errors and finishing script

--- fetch_nutch.bash
+++ fetch_nutch.bash
@@ -63,8 +63,25 @@ then
     exit 1
 fi
 
-echo "Running ant on nutch..."
-cd apache-nutch-${version} && ./../apache-ant-${ant_version}/bin/ant
+echo "Copy ivy/ivy.xml -> ./apache-nutch-${version}/"
+cp ivy/* ./apache-nutch-${version}/ivy/
+
+hbase_version=0.98.8
+echo "Downloading HBase-${hbase_version}"
+curl -sv http://archive.apache.org/dist/hbase/hbase-${hbase_version}/hbase-${hbase_version}-hadoop2-bin.tar.gz -o hbase-${hbase_version}-hadoop2-bin.tar.gz
+curl -sv http://archive.apache.org/dist/hbase/hbase-${hbase_version}/hbase-${hbase_version}-hadoop2-bin.tar.gz.mds -o hbase-${hbase_version}-hadoop2-bin.tar.gz.mds
+
+tar xvfz hbase-${hbase_version}-hadoop2-bin.tar.gz
+succeed=$?
+
+if [ $succeed != 0 ]
+then
+    echo "Can't unpack package hbase-${hbase_version}-hadoop2-bin.tar.gz"
+    exit 1
+fi
+
+echo "Compiling -> nutch"
+cd apache-nutch-${version} && ./../apache-ant-${ant_version}/bin/ant runtime
 succeed=$?
 
 if [ $succeed != 0 ]
@@ -73,13 +90,36 @@ then
     exit 1
 fi
 
+# Return to the script dir
+cd $cwd && pwd
+echo "Copying conf/nutch/* -> ./apache-nutch-${version}/runtime/local/conf"
+cp conf/nutch/* ./apache-nutch-${version}/runtime/local/conf
+
+echo "Copying conf/hbase/* -> ./hbase-${hbase_version}-hadoop2/conf"
+cp conf/hbase/* ./hbase-${hbase_version}-hadoop2/conf
+
+./hbase-${hbase_version}-hadoop2/bin/start-hbase.sh
+echo "Hbase UI Running on http://localhost:16010"
+
+echo "Injecting urls into nutch"
+./apache-nutch-${version}/runtime/local/bin/nutch inject urls
+echo "Generate urls to fetch"
+./apache-nutch-${version}/runtime/local/bin/nutch generate -topN 40
+echo "Fetch pages"
+./apache-nutch-${version}/runtime/local/bin/nutch fetch -all
+echo "Parse pages"
+./apache-nutch-${version}/runtime/local/bin/nutch parse -all
+echo "Running update will keep stuff fresh"
+#./apache-nutch-${version}/runtime/local/bin/nutch updatedb -all
+echo "Indexing with elastic search"
+./apache-nutch-${version}/runtime/local/bin/nutch index elasticsearch -all
+
+
 echo "Cleaning up..."
 cd $cwd
 echo "/bin/rm -f apache-nutch-${version}*gz*"
 /bin/rm -f apache-nutch-${version}*gz*
 echo "/bin/rm -f apache-ant-${ant_version}*gz*"
 /bin/rm -f apache-ant-${ant_version}*gz*
-
-echo "Copying conf/nutch-site.xml -> ./apache-nutch-${version}/runtime/local/conf"
-cp conf/nutch-site.xml ./apache-nutch-${version}/runtime/local/conf
-
+echo "/bin/rm -f hbase-${hbase_version}*gz*"
+/bin/rm -f hbase-${hbase_version}*gz*
